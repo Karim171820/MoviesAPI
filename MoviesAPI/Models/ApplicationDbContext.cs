0edfaae4c1291ea73a91b5223b67e63f2c98a991
karim.mohamed <karim.mohamed@dsquares.com>
2023-03-14 12:19:03 +0200

    Create Movies Database and Genres Table

---
+++ MoviesAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
