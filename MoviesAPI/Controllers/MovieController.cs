e011abccf1145ccc1b21e9f8a531475f583f2fb0
karim.mohamed <karim.mohamed@dsquares.com>
2023-03-15 21:33:42 +0200

    Create Movie with upload Poster-Column (Post Method)

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Dtos;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovie([FromForm]MovieDto dto)
+        {
+            using var DataStream = new MemoryStream();
+            var movie = new Movie
+            {
+                GenreId = dto.GenreId,
+                storyline = dto.storyline,
+                Poster = DataStream.ToArray(),
+                Rate = dto.Rate,
+                Title = dto.Title
+            };
+           await _context.AddAsync(movie);
+            _context.SaveChanges();
+            return Ok(movie);
+
+        }
+
+    }
+}
---
+++ MoviesAPI/Dtos/MovieDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Dtos
+{
+    public class MovieDto
+    {
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public double Rate { get; set; }
+        [MaxLength(500)]
+        public string storyline { get; set; }
+        public IFormFile Poster { get; set; }
+        public byte GenreId { get; set; }
+    }
+}
